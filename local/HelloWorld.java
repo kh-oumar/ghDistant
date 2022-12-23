752cf8a4968477bbf872798b7f7d2f0a082f9890
kh-oumar2 <121313431+kh-oumar2@users.noreply.github.com>
2022-12-23 20:17:42 +0100

    corriger l’issue d’ajout de label de date et changement de taille

--- local/HelloWorld.java
+++ local/HelloWorld.java
@@ -3,10 +3,11 @@ Algo HelloWorld {
  fonction principale {
 	// Création de la fenetre
 	Créer une fenetre de titre HELLO_WORLD
-	Mettre la dimension à 300 , 500
+	Mettre la dimension à 300 , 550
 	Configurer la fenetre sur ‘exitOnClose’
 	// Création du label de texte
 	Créer un label avec du texte centré et contenant le texte HELLO_WORLD
+	Ajouter label date
 	Créer la couleur jaune
 	Mettre la couleur jaune sur le label
 	Mettre la couleur cyan au fond
