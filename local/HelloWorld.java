6831d5fe27b4a0cc87888e3632186e9ff6dd3855
Test Name <test@gmail.com>
2022-12-23 15:18:33 +0100

    C5 : fond d'écran en jaune

--- local/HelloWorld.java
+++ local/HelloWorld.java
@@ -9,6 +9,7 @@ Algo HelloWorld {
 	Créer un label avec du texte centré et contenant le texte HELLO_WORLD
 	Créer la couleur jaune
 	Mettre la couleur jaune sur le label
+	Mettre un couleur jaune au fond 
 	// Finalisation
 	Ajouter le lable sur la fenetre
 	Afficher la fenêtre
