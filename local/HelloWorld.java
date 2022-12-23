1cf6f151545874b3b2b4785436850b51bbcd0fd0
Test Name <test@gmail.com>
2022-12-23 11:27:02 +0100

    C2 : extraction des constantes

--- local/HelloWorld.java
+++ local/HelloWorld.java
@@ -1,15 +1,16 @@
+Utiliser les constantes de Constantes
 Algo HelloWorld {
  fonction principale {
-// Création de la fenetre
- Créer une fenetre de titre ‘Hello world’
-Mettre la dimension à 200, 200
-Configurer la fenetre sur ‘exitOnClose’
-// Création du label de texte
-Créer un label avec du texte centré et contenant le texte ‘Hello world’
-Créer la couleur jaune
-Mettre la couleur jaune sur le label
-// Finalisation
-Ajouter le lable sur la fenetre
-Afficher la fenêtre
+	// Création de la fenetre
+	Créer une fenetre de titre HELLO_WORLD
+	Mettre la dimension à 200, 200
+	Configurer la fenetre sur ‘exitOnClose’
+	// Création du label de texte
+	Créer un label avec du texte centré et contenant le texte HELLO_WORLD
+	Créer la couleur jaune
+	Mettre la couleur jaune sur le label
+	// Finalisation
+	Ajouter le lable sur la fenetre
+	Afficher la fenêtre
 }
 }
